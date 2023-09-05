bddeb55d027259f34669ae68bbef7c4ba1333076
tivt <tivt231@gmail.com>
2023-09-05 17:51:12 -0300

    feat: added parseBranching

--- translator/parseBranching.go
+++ translator/parseBranching.go
@@ -1,5 +1,14 @@
 package translator
 
+import "fmt"
+
 func parseBranching(commands []string) string {
-	return ""
+	cmd1 := commands[1]
+	branching := map[string]string{
+		"label":   fmt.Sprintf("(%v)\n@SP\nM=M+1", cmd1),
+		"goto":    fmt.Sprintf("@%v\n0;JMP", cmd1),
+		"if-goto": fmt.Sprintf("@SP\nAM=M-1\nD=M\n@%v\nD;JLT", cmd1),
+	}
+
+	return branching[commands[0]]
 }
